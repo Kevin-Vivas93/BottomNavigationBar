4d57a42b38ac736d358b966f06f4dc5db8bd3480
Kevin Vivas <kvinloaiza93@gmail.com>
2023-11-07 22:27:35 -0500

    app finish

---
+++ README.md
@@ -0,0 +1,3 @@
+# bottonnavbar
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/bottonnavbar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bottonnavbar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
